37589f999c99840d0fc455e7f9f32f9fb2dcde55
Raphaelli-Lucas <lucasraveloso@gmail.com>
2025-07-21 17:15:07 -0300

    atualização 1

---
+++ assets/script/resultado.js
@@ -0,0 +1,104 @@
+function carregarResultados() {
+  const partidas = JSON.parse(localStorage.getItem("partidas") || "[]");
+  const corpo = document.getElementById("listaResultados");
+  corpo.innerHTML = "";
+
+  partidas.forEach((part, nmrPart) => {
+    const tr = document.createElement("tr");
+
+    let resultado = "Não registrado";
+    if (typeof part.vencedor !== "undefined") {
+      resultado = part.vencedor === "empate"
+        ? "Empate"
+        : `Vitória de ${part.vencedor}`;
+    }
+
+    tr.innerHTML = `
+      <td>${part.data}</td>
+      <td>${part.local}</td>
+      <td>${part.equipeA}</td>
+      <td>${part.pontosA}</td>
+      <td>${part.equipeB}</td>
+      <td>${part.pontosB}</td>
+      <td>${resultado}</td>
+      <td>${part.pontosA ?? "-"} x ${part.pontosB ?? "-"}</td>
+      <td>
+        <button onclick="abrirModalResultado(${nmrPart})">Registrar Resultado</button>
+      </td>
+    `;
+
+    corpo.appendChild(tr);
+  });
+}
+carregarResultados()
+//---                 |
+//funcoes dos modais \/
+let numeroDaPartida = null;
+let partidaAtual = null;
+
+function abrirModalResultado(NmrModal) {
+  const partidas = JSON.parse(localStorage.getItem("partidas") || "[]");
+  const p = partidas[NmrModal];
+  numeroDaPartida = NmrModal;
+  partidaAtual = p;
+
+  document.getElementById("modalResultado").classList.remove("hidden");
+  document.getElementById("infoPartida").textContent = `${p.equipeA} (${p.golsA}) x (${p.golsB}) ${p.equipeB}`;
+
+  const select = document.getElementById("vencedorSelect");
+  select.innerHTML = `
+    <option value="">Qual equipe venceu?</option>
+    <option value="empate">Empate</option>
+    <option value="${p.equipeA}">${p.equipeA}</option>
+    <option value="${p.equipeB}">${p.equipeB}</option>
+  `;
+
+  document.getElementById("pontosA").value = "";
+  document.getElementById("pontosB").value = "";
+}
+3
+//---                |
+// salvar resultado \/
+function salvarResultado() {
+  const vencedor = document.getElementById("vencedorSelect").value;
+  const pontosA = parseInt(document.getElementById("pontosA").value);
+  const pontosB = parseInt(document.getElementById("pontosB").value);
+  const partidas = JSON.parse(localStorage.getItem("partidas") || "[]");
+  console.log(partidas);
+  let ME = document.getElementById("MensagemErro");
+  if (vencedor === "" || isNaN(pontosA) || isNaN(pontosB)) {
+    ME.style.color = "red";
+    ME.style.fontWeight = "bold";
+    ME.innerText = "Por favor, preencha todos os campos corretamente.";
+    return;
+  } else if(vencedor===partidas[numeroDaPartida].equipeA && pontosA<=pontosB ||
+            vencedor === "empate" && pontosA!==pontosB ||
+            vencedor===partidas[numeroDaPartida].equipeB && pontosB<=pontosA
+  ){
+    console.log("Erro de pontuação");
+    
+    ME.style.color = "red";
+    ME.style.fontWeight = "bold";
+    ME.innerText = "A pontuação não confere com o vencedor selecionado!";
+    return;
+  }
+
+  
+  
+  partidas[numeroDaPartida].vencedor = vencedor;
+  partidas[numeroDaPartida].pontosA = pontosA;
+  partidas[numeroDaPartida].pontosB = pontosB;
+
+  localStorage.setItem("partidas", JSON.stringify(partidas));
+  fecharModal();
+  carregarResultados();
+}
+//---          |
+// fchar modal\/
+function fecharModal() {
+  document.getElementById("modalResultado").classList.add("hidden");
+  numeroDaPartida = null;
+  partidaAtual = null;
+}
+//---
+carregarResultados();
